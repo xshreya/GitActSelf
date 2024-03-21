57251c611c6b360a766d5dcc2fabb723cbb79b3b
xshreya <shreyaa.shrmaa@gmail.com>
2024-03-21 14:01:53 +0530

    initial commit

---
+++ file.py
@@ -0,0 +1,10 @@
+# Basic Python script for greeting the user
+
+def main():
+    
+    greeting_message = "Hello! Welcome to the Python script."
+
+    print(greeting_message)
+
+if __name__ == "__main__":
+    main()
